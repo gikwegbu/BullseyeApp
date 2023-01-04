2e44c52c711c1b82fc23e14673b6fcc2c78ab206
George  Ikwegbu Chinedu <39649680+gikwegbu@users.noreply.github.com>
2023-01-04 02:18:15 +0100

    Initial Commit

---
+++ BullseyeApp/BullseyeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeAppApp.swift
+//  BullseyeApp
+//
+//  Created by gikwegbu on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
